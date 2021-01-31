bd0c4e9483e66dacd6fab86150e3a3a9a7f85ade
Vincent Bakker <mail@bakkerv.nl>
2021-01-31 01:00:05 +0100

    Further completed management of enocean devices via OptionFlow

--- homeassistant/components/enocean/options_flow.py
+++ homeassistant/components/enocean/options_flow.py
@@ -3,6 +3,7 @@ from homeassistant.const import (
     CONF_COVERS,
     CONF_DEVICE,
     CONF_ID,
+    CONF_LIGHTS,
     CONF_NAME,
     CONF_SENSORS,
     CONF_SWITCHES,
@@ -15,6 +16,7 @@ import copy
 
 
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import device_registry
 from homeassistant.helpers.device_registry import (
     async_entries_for_config_entry,
     async_get_registry as async_get_device_registry,
@@ -44,19 +46,22 @@ from .sensor import (
 )
 
 from .switch import CONF_CHANNEL, SWITCH_SCHEMA, SWITCH_ALL_CHANNELS
+from .light import LIGHT_SCHEMA
 
 _LOGGER = logging.getLogger(__name__)
 
 
+ADD_KEY = "add_selection"
 EDIT_KEY = "edit_selection"
-ADD_COVER = "Add cover"
-EDIT_COVER = "Edit cover"
-ADD_SENSOR = "Add sensor"
-EDIT_SENSOR = "Edit sensor"
-ADD_SWITCH = "Add switch"
-EDIT_SWITCH = "Edit switch"
+REMOVE_KEY = "remove_selection"
 
 CONF_ALL_CHANNELS = "all_channels"
+
+PLATFORM_LIGHT = "light"
+PLATFORM_SENSOR = "sensor"
+PLATFORM_COVER = "cover"
+PLATFORM_SWITCH = "switch"
+PLATFORMS = [PLATFORM_SENSOR, PLATFORM_COVER, PLATFORM_LIGHT, PLATFORM_SWITCH]
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
@@ -82,33 +87,41 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
-            if user_input[EDIT_KEY] == ADD_COVER:
-                return await self.async_step_create_cover()
-            if user_input[EDIT_KEY] == ADD_SWITCH:
-                return await self.async_step_create_switch()
-            if user_input[EDIT_KEY] == ADD_SENSOR:
-                return await self.async_step_create_sensor()
-            if user_input[EDIT_KEY] == EDIT_SENSOR:
-                return await self.async_step_select_sensor()
-            if user_input[EDIT_KEY] == EDIT_COVER:
-                return await self.async_step_select_cover()
-            if user_input[EDIT_KEY] == EDIT_SWITCH:
-                return await self.async_step_select_switch()
+            if ADD_KEY in user_input:
+                if user_input[ADD_KEY] == PLATFORM_COVER:
+                    return await self.async_step_create_cover()
+                if user_input[ADD_KEY] == PLATFORM_SWITCH:
+                    return await self.async_step_create_switch()
+                if user_input[ADD_KEY] == PLATFORM_SENSOR:
+                    return await self.async_step_create_sensor()
+                if user_input[ADD_KEY] == PLATFORM_LIGHT:
+                    return await self.async_step_create_light()
+            if EDIT_KEY in user_input:
+                if user_input[EDIT_KEY] == PLATFORM_SENSOR:
+                    return await self.async_step_select_sensor()
+                if user_input[EDIT_KEY] == PLATFORM_COVER:
+                    return await self.async_step_select_cover()
+                if user_input[EDIT_KEY] == PLATFORM_SWITCH:
+                    return await self.async_step_select_switch()
+                if user_input[EDIT_KEY] == PLATFORM_LIGHT:
+                    return await self.async_step_select_light()
+            if REMOVE_KEY in user_input:
+                if user_input[REMOVE_KEY] == PLATFORM_SENSOR:
+                    return await self.async_step_select_sensor_to_remove()
+                if user_input[REMOVE_KEY] == PLATFORM_COVER:
+                    return await self.async_step_select_cover_to_remove()
+                if user_input[REMOVE_KEY] == PLATFORM_SWITCH:
+                    return await self.async_step_select_switch_to_remove()
+                if user_input[REMOVE_KEY] == PLATFORM_LIGHT:
+                    return await self.async_step_select_light_to_remove()
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema(
                 {
-                    vol.Required(EDIT_KEY): vol.In(
-                        [
-                            ADD_COVER,
-                            EDIT_COVER,
-                            ADD_SENSOR,
-                            EDIT_SENSOR,
-                            ADD_SWITCH,
-                            EDIT_SWITCH,
-                        ]
-                    )
+                    vol.Optional(ADD_KEY): vol.In(PLATFORMS),
+                    vol.Optional(EDIT_KEY): vol.In(PLATFORMS),
+                    vol.Optional(REMOVE_KEY): vol.In(PLATFORMS),
                 },
             ),
         )
@@ -153,6 +166,42 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         )
         return self.async_show_form(
             step_id="create_cover", data_schema=schema, errors=errors
+        )
+
+    async def async_step_create_light(self, user_input=None):
+        """Add new light step"""
+        errors = {}
+        if user_input is not None:
+            dev_id = self.device_id_or_none(user_input[CONF_ID])
+            if dev_id is None:
+                errors[CONF_ID] = "invalid_id"
+            sender_id = self.device_id_or_none(user_input[CONF_SENDER_ID])
+            if sender_id is None:
+                errors[CONF_SENDER_ID] = "invalid_sender_id"
+            user_input[CONF_ID] = dev_id
+            user_input[CONF_SENDER_ID] = sender_id
+            try:
+                if not errors:
+                    data = LIGHT_SCHEMA(user_input)
+            except vol.error.Error:
+                errors["base"] = "invalid_data"
+            if not errors:
+                self.update_config_data(lights={to_hex_string(dev_id): data})  # type: ignore
+                return self.async_create_entry(title="", data=None)
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_ID, description={"suggested_value": "0xff,0xff,0xff,0xff"}
+                ): cv.string,
+                vol.Required(
+                    CONF_SENDER_ID,
+                    description={"suggested_value": "0xff,0xff,0xff,0xff"},
+                ): cv.string,
+                vol.Required(CONF_NAME): cv.string,
+            }
+        )
+        return self.async_show_form(
+            step_id="create_light", data_schema=schema, errors=errors
         )
 
     async def async_step_create_sensor(self, user_input=None):
@@ -232,11 +281,62 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         )
 
     async def async_step_select_cover(self, user_input=None):
-        """Manage the options."""
+        return await self.async_step_select_device(
+            "cover",
+            self.async_step_set_cover_options,
+            "select_cover",
+            user_input=user_input,
+        )
+
+    async def async_step_select_cover_to_remove(self, user_input=None):
+        return await self.async_step_select_device(
+            "cover",
+            self.async_step_remove_cover,
+            "select_cover_to_remove",
+            user_input=user_input,
+        )
+
+    async def async_step_select_switch_to_remove(self, user_input=None):
+        return await self.async_step_select_device(
+            "switch",
+            self.async_step_remove_switch,
+            "select_switch_to_remove",
+            user_input=user_input,
+        )
+
+    async def async_step_select_light_to_remove(self, user_input=None):
+        return await self.async_step_select_device(
+            "light",
+            self.async_step_remove_light,
+            "select_light_to_remove",
+            user_input=user_input,
+        )
+
+    async def async_step_select_sensor_to_remove(self, user_input=None):
+        return await self.async_step_select_device(
+            "sensor",
+            self.async_step_remove_sensor,
+            "select_sensor_to_remove",
+            user_input=user_input,
+        )
+
+    async def async_step_select_light(self, user_input=None):
+        return await self.async_step_select_device(
+            "light",
+            self.async_step_set_light_options,
+            "select_light",
+            user_input=user_input,
+        )
+
+    async def async_step_select_device(
+        self, device_type, set_options_step, step_id, user_input=None
+    ):
+        """Select a device."""
+        DEVICE = "device"
         if user_input is not None:
-            if CONF_COVERS in user_input:
-                self._selected_device_entry_id = user_input[CONF_COVERS]
-                return await self.async_step_set_cover_options()
+            if DEVICE in user_input:
+                self._selected_device_entry_id = user_input[DEVICE]
+                return await set_options_step()
 
             return self.async_create_entry(title="", data=user_input)
 
@@ -247,81 +347,35 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         self._device_registry = device_registry
         self._device_entries = device_entries
 
-        configure_covers = {
+        configure_devices = {
             entry.id: entry.name_by_user if entry.name_by_user else entry.name
             for entry in device_entries
-            if self.is_device_type(entry.identifiers, "cover")
+            if self.is_device_type(entry.identifiers, device_type)
         }
 
         return self.async_show_form(
-            step_id="select_cover",
+            step_id=step_id,
             data_schema=vol.Schema(
                 {
-                    vol.Optional(CONF_COVERS): vol.In(configure_covers),
+                    vol.Optional(DEVICE): vol.In(configure_devices),
                 }
             ),
         )
 
     async def async_step_select_sensor(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            if CONF_SENSORS in user_input:
-                self._selected_device_entry_id = user_input[CONF_SENSORS]
-                return await self.async_step_set_sensor_options()
-
-            return self.async_create_entry(title="", data=user_input)
-
-        device_registry = await async_get_device_registry(self.hass)
-        device_entries = async_entries_for_config_entry(
-            device_registry, self._config_entry.entry_id
-        )
-        self._device_registry = device_registry
-        self._device_entries = device_entries
-
-        configure_sensors = {
-            entry.id: entry.name_by_user if entry.name_by_user else entry.name
-            for entry in device_entries
-            if self.is_device_type(entry.identifiers, "sensor")
-        }
-
-        return self.async_show_form(
-            step_id="select_sensor",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_SENSORS): vol.In(configure_sensors),
-                }
-            ),
+        return await self.async_step_select_device(
+            "sensor",
+            self.async_step_set_sensor_options,
+            "select_sensor",
+            user_input=user_input,
         )
 
     async def async_step_select_switch(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            if CONF_SWITCHES in user_input:
-                self._selected_device_entry_id = user_input[CONF_SWITCHES]
-                return await self.async_step_set_switch_options()
-
-            return self.async_create_entry(title="", data=user_input)
-
-        device_registry = await async_get_device_registry(self.hass)
-        device_entries = async_entries_for_config_entry(
-            device_registry, self._config_entry.entry_id
-        )
-        self._device_registry = device_registry
-        self._device_entries = device_entries
-
-        configure_switches = {
-            entry.id: entry.name_by_user if entry.name_by_user else entry.name
-            for entry in device_entries
-            if self.is_device_type(entry.identifiers, "switch")
-        }
-
-        return self.async_show_form(
-            step_id="select_switch",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_SWITCHES): vol.In(configure_switches),
-                }
-            ),
+        return await self.async_step_select_device(
+            "switch",
+            self.async_step_set_switch_options,
+            "select_switch",
+            user_input=user_input,
         )
 
     def find_matching_config(self, device_type, config_key):
@@ -354,6 +408,35 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         except ValueError:
             return None
         return sender_id
+
+    async def async_step_remove_cover(self, user_input=None):
+        self.find_matching_config("cover", CONF_COVERS)
+        if self._current_dev_config_key is not None:
+            self._device_registry.async_remove_device(self._selected_device_entry_id)
+            self.update_config_data(covers={self._current_dev_config_key: None})
+
+        return self.async_create_entry(title="", data=None)
+
+    async def async_step_remove_switch(self, user_input=None):
+        self.find_matching_config("switch", CONF_SWITCHES)
+        if self._current_dev_config_key is not None:
+            self._device_registry.async_remove_device(self._selected_device_entry_id)
+            self.update_config_data(switches={self._current_dev_config_key: None})
+        return self.async_create_entry(title="", data=None)
+
+    async def async_step_remove_light(self, user_input=None):
+        self.find_matching_config("light", CONF_LIGHTS)
+        if self._current_dev_config_key is not None:
+            self._device_registry.async_remove_device(self._selected_device_entry_id)
+            self.update_config_data(lights={self._current_dev_config_key: None})
+        return self.async_create_entry(title="", data=None)
+
+    async def async_step_remove_sensor(self, user_input=None):
+        self.find_matching_config("sensor", CONF_SENSORS)
+        if self._current_dev_config_key is not None:
+            self._device_registry.async_remove_device(self._selected_device_entry_id)
+            self.update_config_data(sensors={self._current_dev_config_key: None})
+        return self.async_create_entry(title="", data=None)
 
     async def async_step_set_cover_options(self, user_input=None):
         errors = {}
@@ -502,16 +585,62 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             )
         return self.async_create_entry(title="", data=None)
 
+    async def async_step_set_light_options(self, user_input=None):
+        errors = {}
+        self.find_matching_config("light", CONF_LIGHTS)
+        if user_input is not None and self._current_dev_config_key is not None:
+            # Test schema
+            try:
+                user_input[CONF_ID] = self._current_dev_config[CONF_ID]
+                sender_id = self.device_id_or_none(user_input[CONF_SENDER_ID])
+                if sender_id is None:
+                    errors[CONF_SENDER_ID] = "invalid_sender_id"
+                user_input[CONF_SENDER_ID] = sender_id
+                data = LIGHT_SCHEMA(user_input)
+            except vol.error.Error:
+                errors["base"] = "invalid_data"
+            if not errors:
+                self.update_config_data(lights={self._current_dev_config_key: data})  # type: ignore
+                return self.async_create_entry(title="", data=None)
+        if self._current_dev_config is not None:
+            schema = vol.Schema(
+                {
+                    vol.Required(
+                        CONF_SENDER_ID,
+                        default=",".join(
+                            map(
+                                lambda x: hex(x),
+                                self._current_dev_config[CONF_SENDER_ID],
+                            )
+                        ),
+                    ): cv.string,
+                    vol.Required(
+                        CONF_NAME, default=self._current_dev_config[CONF_NAME]
+                    ): cv.string,
+                }
+            )
+            return self.async_show_form(
+                step_id="set_light_options", data_schema=schema, errors=errors
+            )
+        return self.async_create_entry(title="", data=None)
+
     @callback
     def update_config_data(
-        self, global_options=None, covers=None, sensors=None, switches=None
+        self,
+        global_options=None,
+        covers=None,
+        sensors=None,
+        switches=None,
+        lights=None,
     ):
         """Update data in ConfigEntry."""
+        removal = False
         entry_data = self._config_entry.data.copy()
         for platform, values in {
             CONF_COVERS: covers,
             CONF_SENSORS: sensors,
             CONF_SWITCHES: switches,
+            CONF_LIGHTS: lights,
         }.items():
             entry_data[platform] = copy.deepcopy(
                 self._config_entry.data.get(platform, {})
@@ -520,9 +649,11 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                 for id, options in values.items():
                     if options is None:
                         entry_data[platform].pop(id)
+                        removal = True
                     else:
                         entry_data[platform][id] = options
         self.hass.config_entries.async_update_entry(self._config_entry, data=entry_data)
-        self.hass.async_create_task(
-            self.hass.config_entries.async_reload(self._config_entry.entry_id)
-        )
+        if not removal:
+            self.hass.async_create_task(
+                self.hass.config_entries.async_reload(self._config_entry.entry_id)
+            )
